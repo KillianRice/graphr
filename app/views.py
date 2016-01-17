da154860746077e4c30be19c55a17a2d00d7990d
joewhalen <jwhalen.physics@gmail.com>
2016-01-17 17:38:45 -0600

    wrote some js

--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from app import app
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, json, jsonify
 from graphr import graphr
+from numpy import random
 
 def getplot():
     f = 'data/data.txt'
     img_path = 'static/graph.svg'
     img_data = graphr.graph_data(f,img_path)
     return img_data
+def makejson(data):
+    return jsonify(data)
+
 
 @app.route('/')
 @app.route('/index', methods = ['GET','POST'])
@@ -18,3 +22,25 @@ def index():
 def update():
     img_data = getplot()
     return img_data
+
+@app.route('/get_json')
+def get_json():
+    data = {
+            'x': random.randn(),
+            'y': random.randn()
+        }
+
+    return jsonify(**data)
+
+@app.route('/js')
+def js():
+    return render_template('js.html')
+
+
+
+
+
+
+
+
+
