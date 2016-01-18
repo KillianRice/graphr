266363fef328a02cf1097047d2a8d98d7d15b10c
joewhalen <jwhalen.physics@gmail.com>
2016-01-17 23:27:26 -0600

    improved a lot of things

--- app/static/plot.js
+++ app/static/plot.js
@@ -1,91 +1,136 @@
 var plotData = []
 var plotstyle = {
-    margin : { t : 10, r : 0, b : 40 }
+    margin : { t : 10, r : 0, b : 40 },
+    legend : { x : 0, y : 1}
 }
 var totalHeight = 90;
 var maxplots = 6;
 
 var gettingData = null;
 
+var allItems = [] // master list of items to get from the server
+var plotItems = {} // the items to be plotted on each plot (denoted by its plotID)
+var plotData = {} // the data for each of the items in allItems
+
 $( function(){
     
+
     var numPlots = 0;
     //Plotly.newPlot('plotNum1',data);
     $('button#get_data').click( function(){
+        gettingData = setInterval( function(){
+            $.getJSON('/get_json',
+            {
+                n : numPlots,
+                i : allItems
+            })
+            .done( function(data){
+                console.log(data);
 
-            gettingData = setInterval( function(){
+                // Extract the new data from the server's response
+                for (i=0; i<allItems.length; i++){
+                    var last = plotData[allItems[i]]['x'].length - 1;
+                    plotData[allItems[i]]['x'].push( data['x'] )
 
-                while (plotData.length < numPlots) {
-                    plotData.push( { x:[], y:[] });
+                    if (plotData[allItems[i]]['y'].length > 0)
+                    {
+                        var newpoint = plotData[allItems[i]]['y'][last] + data[allItems[i]];
+                        plotData[allItems[i]]['y'].push(newpoint);
+                    } else {
+                        plotData[allItems[i]]['y'].push( data[allItems[i]]);
+                    }
                 }
-
-                $.getJSON('/get_json',
-                    { n : numPlots })
-                    .done( function(data){
-                        var last = 0;
-                        for (i=0; i<numPlots; i++){
-                            last = plotData[i]['y'].length-1;
-                            plotData[i]['x'].push(data['x']);
-                            if (last < 0){
-                                plotData[i]['y'].push(data['y'][i]);
-                            } else {
-                                var newpoint = plotData[i]['y'][last] + data['y'][i];
-                                plotData[i]['y'].push(newpoint);
-                            }
-                            Plotly.redraw('plotNum'+(i+1), plotstyle);
-                        }
-                    });
-
-                //Plotly.redraw('plotNum1');
-            
-            } , 100);
-        
-        });
-
-    $('button#add_plot').click( function(){
-
-        numPlots++;
-        var plotID = 'plotNum' + (numPlots);
-        var plotdiv = '<div class="plot" id="' + plotID + '"></div>';
-
-        if (numPlots == 1){
-            $('div#plots').html(plotdiv)
-        } else {
-            $('div#plots').append(plotdiv)
-        }
-        
-        if (plotData.length < numPlots){
-            plotData.push( { x : [], y : [] } );
-        }
-        Plotly.newPlot(plotID, [plotData[numPlots-1]], plotstyle);
-
-        
-        if (numPlots <= maxplots){
-            $('div.plot').height(totalHeight / numPlots +'%');
-            plotstyle['height'] = totalHeight / numPlots + '%';
-
-            for (i=1; i<=numPlots; i++){
-                Plotly.relayout('plotNum'+i, plotstyle);
-            }
-        } else {
-            //$('div.plot').height(totalHeight / maxplots + '%');
-            //plotstyle['height'] = totalHeight / maxplots + '%';
-            PLotly.relayout(plotID, plotstyle);
-        }
-
-        $('button#stop').click( function(){
-            clearInterval(gettingData);
-        });
-
-        $('button#clear').click( function(){
-            clearInterval(gettingData);
-            for (i=0; i<numPlots; i++){
-                plotData[i]['x'] = [];
-                plotData[i]['y'] = [];
-            }
-            $('button#get_data').click()
-        });
+                
+                // loop over the plots and update them
+                for (i=0; i<numPlots; i++) {
+                    plotID = 'plotNum'+(i+1);
+                    Plotly.redraw(plotID, plotstyle);
+                }
+                 
+            });
+        }, 500);
     });
+
+    $('button#stop').click( function(){
+        clearInterval(gettingData);
+    });
+
+    $('button#clear').click( function(){
+        clearInterval(gettingData);
+        for (i=0; i<allItems.length; i++){
+            plotData[allItems[i]]['x'] = [];
+            plotData[allItems[i]]['y'] = [];
+        }
+        $('button#get_data').click()
+    });
+
+    $('#add_plot').click( function(){
+
+        // Figure out what items belong to this plot
+        var items = []
+        var someChecked = false;
+        $('.plot_item').each( function(){
+            if($(this).is(":checked")) {
+                console.log($(this).attr('name'));
+                items.push($(this).attr('name'));
+                someChecked = true;
+                $(this).prop('checked', false);
+            }
+        });
+
+        if (someChecked){
+
+            // check that the items are in the master list
+            for (i=0; i<items.length; i++){
+                console.log($.inArray(items[i],allItems))
+                if ($.inArray(items[i],allItems) == -1){
+                    allItems.push(items[i]);
+                    plotData[items[i]] = {
+                        x       : [],
+                        y       : [],
+                        name    : items[i]
+                    };
+                }
+            }
+            
+            console.log(allItems)
+            // create the plot
+            numPlots++;
+            var plotID = 'plotNum' + (numPlots);
+            var plotdiv = '<div class="plot" id="' + plotID + '"></div>';
+            plotItems[plotID] = items;
+
+            if (numPlots == 1){
+                $('div#plots').html(plotdiv)
+            } else {
+                $('div#plots').append(plotdiv)
+            }
+            
+            var theData = []
+            for (i=0; i<items.length; i++){
+                theData.push(plotData[items[i]]);
+            }
+            Plotly.newPlot(plotID, theData, plotstyle);
+
+            
+            if (numPlots <= maxplots){
+                $('div.plot').height(totalHeight / numPlots +'%');
+                plotstyle['height'] = totalHeight / numPlots + '%';
+
+                for (i=1; i<=numPlots; i++){
+                    Plotly.relayout('plotNum'+i, plotstyle);
+                }
+            } else {
+                //$('div.plot').height(totalHeight / maxplots + '%');
+                //plotstyle['height'] = totalHeight / maxplots + '%';
+                PLotly.relayout(plotID, plotstyle);
+            }
+        }
+            
+    });
+
+    
+
  });
 
 
--- app/views.py
+++ app/views.py
@@ -3,14 +3,17 @@ from flask import render_template, flash, redirect, request, json, jsonify
 from graphr import graphr
 from numpy import random
 from datetime import datetime
+import verdi_config
+
+c = sorted(verdi_config.COMMANDS.keys())[:6]
+
+#c = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 def getplot():
     f = 'data/data.txt'
     img_path = 'static/graph.svg'
     img_data = graphr.graph_data(f,img_path)
     return img_data
-def makejson(data):
-    return jsonify(data)
 
 
 @app.route('/')
@@ -28,16 +31,17 @@ def update():
 def get_json():
     datefmt = '%Y-%m-%d %H:%M:%S.%f'
     n = request.args.get('n', 0, type=int)
-    data = {
-            'x': datetime.now().strftime(datefmt),
-            'y': [ random.randn() for i in range(n) ]
-        }
+    names = request.args.getlist('i[]')
+    data = {}
+    data['x'] = datetime.now().strftime(datefmt)
+    for name in names:
+        data[name] = random.randn()
 
     return jsonify(**data)
 
 @app.route('/js')
 def js():
-    return render_template('js.html')
+    return render_template('js.html', commands = c)
 
 
 
